cf9f1b8955f740762a6d34542c1f3de13b0e7678
Dean Burdaky <dean.burdaky@dematic.com>
2022-09-01 16:55:06 +0100

    Started work on the query extractor and matcher

--- mockwebserver/extraction/extractor.py
+++ mockwebserver/extraction/extractor.py
@@ -3,7 +3,8 @@
 #   matcher-based extractor class
 # the user will likely implement their own request data extraction code
 
-from typing import Optional, Sequence, Dict, Any
+import re
+from typing import Optional, Sequence, Dict, Any, Union
 
 from twisted.web.server import Request as Tw_Request
 from parse import parse as Pa_parse, Result as Pa_Result
@@ -38,3 +39,51 @@ class PathExtractor (Extractor):
       path += '/'
     result = Pa_parse(pattern, path, case_sensitive=True)
     return Extract(list(result.fixed), result.named) if isinstance(result, Pa_Result) else None
+
+class QueryExtractor (Extractor):
+  def __init__(self, matchSubset : bool = True, **parameters : Sequence[str]):
+    self.matchSubset = matchSubset
+    self.parameters = parameters
+
+  def getId(self) -> str:
+    return "QUERY {}".format(self.parameters)
+
+  def extractData(self, request: Tw_Request) -> Optional[Extract]:
+    if request == None:
+      raise ValueError()
+    elif len(self.parameters) > len(request.args):
+      return None
+    elif not self.matchSubset and len(self.parameters) < len(request.args):
+      return None
+    params = request.args.copy()
+    extract = Extract([], {})
+    for patternKey in self.parameters:
+      keyResult = None
+      valueResult = None
+      matchingRequestKey = None
+      for requestKey in params:
+        keyResult = Pa_parse(patternKey, requestKey, case_sensitive=True)
+        if not isinstance(keyResult, Pa_Result):
+          continue
+        patternValues = self.parameters[patternKey]
+        requestValues = params[requestKey]
+        if len(patternValues) > len(requestValues):
+          continue
+        elif not self.matchSubset and len(patternValues) < len(requestValues):
+          continue
+        for index in range(len(patternValues)):
+          valueResult = Pa_parse(patternValues[index], requestValues[index], case_sensitive=True)
+          if isinstance(valueResult, Pa_Result):
+            matchingRequestKey = requestKey
+            break
+      if isinstance(keyResult, Pa_Result) and isinstance(valueResult, Pa_Result) and params.pop(matchingRequestKey, None) != None:
+        extract.fixed.extend(list(keyResult.fixed))
+        extract.fixed.extend(list(valueResult.fixed))
+        extract.named.update(keyResult.named)
+        extract.named.update(valueResult.named)
+      else:
+        return None
+    return extract
+      
+
+
--- mockwebserver/matching/matcher.py
+++ mockwebserver/matching/matcher.py
@@ -4,11 +4,12 @@
 #   query matcher
 #   method matcher
 
-from typing import Type
+from enum import Enum
+from typing import Sequence, Union
 
 from twisted.web.server import Request as Tw_Request
 
-from mockwebserver.extraction.extractor import Extract, Extractor, PathExtractor
+from mockwebserver.extraction.extractor import Extract, Extractor, PathExtractor, QueryExtractor
 
 class Matcher:
   def __init__(self, extractor : Extractor):
@@ -25,3 +26,34 @@ class PathMatcher (Matcher):
   def __init__(self, pattern : str):
     super().__init__(PathExtractor(pattern))
     self.pattern = pattern
+
+class Method (Enum):
+  GET = "GET",
+  HEAD = "HEAD",
+  POST = "POST",
+  PUT = "PUT",
+  DELETE = "DELETE",
+  CONNECT = "CONNECT",
+  OPTIONS = "OPTIONS",
+  TRACE = "TRACE",
+  PATCH = "PATCH"
+
+class MethodMatcher (Matcher):
+  def __init__(self, method : Union[Method, str]):
+    super().__init__(None)
+    if isinstance(method, str):
+      method = Method(method)
+    elif method == None:
+      raise ValueError()
+    self.method = method
+
+  def getId(self) -> str:
+    return "METHOD {}".format(self.method.value)
+
+  def matchesRequest(self, request: Tw_Request) -> bool:
+    return request.method == bytes(self.method.value)
+
+class QueryMatcher (Matcher):
+  def __init__(self, **parameters : Sequence[str]):
+    super().__init__(QueryExtractor(**parameters))
+    self.parameters = parameters
